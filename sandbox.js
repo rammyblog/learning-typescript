2151175bdc2ef1b0172648421d2e060560fbb8aa
Onasanya Tunde <onasanyatunde67@gmail.com>
2021-02-14 23:20:38 +0100

    Type bascis

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,14 @@
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// const inputs = document.querySelectorAll('input')
+// inputs.forEach(input => {
+//     console.log(input);
+// })
+// Type Basics
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// Error
+// character = 50
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,19 @@
-const inputs = document.querySelectorAll('input')
-inputs.forEach(input => {
-    console.log(input);
+// const inputs = document.querySelectorAll('input')
+// inputs.forEach(input => {
+//     console.log(input);
     
-})
+// })
+
+
+// Type Basics
+let character = 'mario'
+let age = 30
+let isBlackBelt = false
+
+// Error
+// character = 50
+
+const circ = (diameter:number) => {
+    return diameter * Math.PI
+}
+console.log(circ(7.5))
