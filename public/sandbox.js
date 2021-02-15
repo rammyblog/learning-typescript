f3c0ed32f22010667dc6bf78830c7a1fd48b2ca4
Onasanya Tunde <onasanyatunde67@gmail.com>
2021-02-16 00:54:35 +0100

    dom and type casting

--- public/sandbox.js
+++ public/sandbox.js
@@ -78,20 +78,20 @@
 // }
 // Function signatures
 // let greet : Function
-var greet;
-greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
-};
-var calc;
-calc = function (numOne, numTwo, action) {
-    if (action == 'add') {
-        return numOne + numTwo;
-    }
-    else {
-        return numOne - numTwo;
-    }
-};
-var logDetails;
-logDetails = function (ninja) {
-    console.log(ninja.name + " is " + ninja.age + " years old");
-};
+// let greet:(a:string, b:string) => void;
+// greet = (name:string, greeting:string) => {
+//     console.log(`${name} says ${greeting}`);
+// }
+// let calc:(a:number, b:number, c:string) =>number
+// calc = (numOne:number, numTwo:number, action:string) => {
+//     if(action == 'add'){
+//         return numOne+numTwo
+//     }else{
+//         return numOne - numTwo
+//     }
+// }
+// let logDetails : (obj:{name:string, age:number}) => void;
+// type person ={name:string, age:number}
+// logDetails = (ninja:person) => {
+//     console.log(`${ninja.name} is ${ninja.age} years old`);
+// }
