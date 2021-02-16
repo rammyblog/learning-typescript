9e15cc2509374a8a999f9166a1ad5a743bbf9653
Onasanya Tunde <onasanyatunde67@gmail.com>
2021-02-16 01:02:51 +0100

    Classes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,33 @@
 "use strict";
-var anchor = document.querySelector('a');
-console.log(anchor.href);
-// const form = document.querySelector('form')!
-var form = document.querySelector('.new-item-form');
-// console.log(form.children)
-// inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+// const anchor = document.querySelector('a')!
+// console.log(anchor.href)
+// // const form = document.querySelector('form')!
+// const form = document.querySelector('.new-item-form') as HTMLFormElement
+// // console.log(form.children)
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+// const details = document.querySelector('#details') as HTMLInputElement
+// const amount = document.querySelector('#amount') as HTMLInputElement
+// form.addEventListener('submit', (e:Event)=>{
+//     e.preventDefault()
+//     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+// })
+// Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('yuhsi', 'work on the yuhsi website', 300);
+// console.log(invOne.format(), invTwo);
+var invoices = [];
+invoices.push(invOne);
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,43 @@
-const anchor = document.querySelector('a')!
-console.log(anchor.href)
-// const form = document.querySelector('form')!
-const form = document.querySelector('.new-item-form') as HTMLFormElement
-// console.log(form.children)
+// const anchor = document.querySelector('a')!
+// console.log(anchor.href)
+// // const form = document.querySelector('form')!
+// const form = document.querySelector('.new-item-form') as HTMLFormElement
+// // console.log(form.children)
 
-// inputs
-const type = document.querySelector('#type') as HTMLSelectElement
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
-const details = document.querySelector('#details') as HTMLInputElement
-const amount = document.querySelector('#amount') as HTMLInputElement
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+// const details = document.querySelector('#details') as HTMLInputElement
+// const amount = document.querySelector('#amount') as HTMLInputElement
 
-form.addEventListener('submit', (e:Event)=>{
-    e.preventDefault()
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+// form.addEventListener('submit', (e:Event)=>{
+//     e.preventDefault()
+//     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
     
-})
+// })
+
+// Classes
+
+class Invoice{
+    client:string;
+    details:string;
+    amount:number
+
+    constructor(client:string, details:string, amount:number){
+        this.client = client
+        this.details=details
+        this.amount=amount
+
+    }
+    format(){
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250)
+const invTwo= new Invoice('yuhsi', 'work on the yuhsi website', 300)
+
+// console.log(invOne.format(), invTwo);
+let invoices: Invoice[]= []
+invoices.push(invOne)
+console.log(invoices);
